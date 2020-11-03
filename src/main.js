9f52531098fa6ee16d9c5a12d9b934d791fe8eb0
piotrfejklowicz <piotrf5bin@o2.pl>
2020-11-03 21:49:42 +0100

    Fixed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 
 const DistanceCalculator = require('distance-calculator-js');
 
-
 // => Signed decimal degrees without compass direction
 const Tokyo = { lat: 35.652832, long: 139.839478 };
 const NewYork = { lat: 40.730610, long: -73.935242 };
